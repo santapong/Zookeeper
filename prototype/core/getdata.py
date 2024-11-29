edd600352a7c443985fbb6d4980454e7b66017f0
Santapong Sondhi <santapongsondhi@gmail.com>
2024-11-29 15:48:09 +0700

    Make Decision to get weather data

---
+++ prototype/core/getdata.py
@@ -0,0 +1,6 @@
+import requests
+import json
+from pprint import pprint
+
+response = requests.get("https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m")
+pprint(response.json())
